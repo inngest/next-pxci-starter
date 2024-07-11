f615acbd5c79af2b33d5f51a741eabc99163eed3
Abukstech <abdulmajeedbamidele4@gmail.com>
2024-07-11 23:33:35 +0100

    added the create event

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -6,7 +6,7 @@ import {
 } from "@clerk/nextjs";
 import Image from "next/image";
 import React from "react";
-import logo from "../public/images/Frame 44 (1).png";
+import logo from "../../public/images/Frame 44 (1).png";
 import Link from "next/link";
 
 const Navbar = () => {
